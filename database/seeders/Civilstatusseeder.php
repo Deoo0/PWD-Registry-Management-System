00fca764b7612064e3bc157ce6c1297c42791559
Lawrence Deo Bersales <lawrencebersales8@gmail.com>
2026-06-24 11:27:54 +0800

    Duplicate Data Seeder Bug Fixed

--- database/seeders/Civilstatusseeder.php
+++ database/seeders/Civilstatusseeder.php
@@ -9,7 +9,7 @@ class CivilStatusSeeder extends Seeder
 {
     public function run(): void
     {
-        DB::table('civil_status')->insert([
+        DB::table('civil_status')->insertOrIgnore([
             ['name' => 'Single', 'created_at' => now(), 'updated_at' => now()],
             ['name' => 'Married', 'created_at' => now(), 'updated_at' => now()],
             ['name' => 'Widowed', 'created_at' => now(), 'updated_at' => now()],
